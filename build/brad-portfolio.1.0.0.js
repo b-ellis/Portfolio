4c53a0c7c9648fbfcd71c5e240bf088df8aa304f
b-ellis <bradjellis18@gmail.com>
2017-04-03 09:22:19 -0700

    new build

--- build/brad-portfolio.1.0.0.js
+++ build/brad-portfolio.1.0.0.js
@@ -45,6 +45,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(1);
+	
 	var displayContentHover = function(){
 		$('.display-container').mouseover(function(){
 			$('.link-container').show();
@@ -84,12 +85,15 @@
 			this.giturl = giturl;
 		};
 	
-		var phish = new Project('../images/phish-quiz.png', 'https://b-ellis.github.io/Quiz/', 'https://github.com/b-ellis/Quiz');
-		var toe = new Project('../images/tic-tac-toe.png', 'https://b-ellis.github.io/Tic-Tac-Toe/', 'https://github.com/b-ellis/Tic-Tac-Toe');
-		var list = new Project('../images/shoppinglist.png', 'https://b-ellis.github.io/Shoppinglist/', 'https://github.com/b-ellis/Shoppinglist');
+		// var phish = new Project('../images/phish-quiz.png', 'https://b-ellis.github.io/Quiz/', 'https://github.com/b-ellis/Quiz');
+		// var toe = new Project('../images/tic-tac-toe.png', 'https://b-ellis.github.io/Tic-Tac-Toe/', 'https://github.com/b-ellis/Tic-Tac-Toe');
+		// var list = new Project('../images/shoppinglist.png', 'https://b-ellis.github.io/Shoppinglist/', 'https://github.com/b-ellis/Shoppinglist');
 		var jam = new Project('../images/jam-location.png', 'https://b-ellis.github.io/Jam-Location/', 'https://github.com/b-ellis/Jam-Location/tree/gh-pages');
+		var Fullstack = new Project('../images/Fullstack-Capstone.png', 'https://thinkfulcapstone.herokuapp.com', 'https://github.com/b-ellis/Fullstack-capstone');
+		var Node = new Project('../images/Node-Capstone.png','https://salty-hollows-61107.herokuapp.com/', 'https://github.com/b-ellis/node-capstone');
+		var React = new Project('../images/React-Capstone.png','https://react-capstone.herokuapp.com/','https://github.com/b-ellis/react-capstone');
 	
-		var projects = [phish, jam, toe, list];
+		var projects = [jam, Node, React, Fullstack];
 		var currentProjectIndex = 0;
 	
 		displayProjects();
