1c05436b6b9481f75e8cce3cb80715125e68951c
b-ellis <bradjellis18@gmail.com>
2017-04-11 15:43:06 -0700

    media quieries added

--- build/brad-portfolio.1.0.0.js
+++ build/brad-portfolio.1.0.0.js
@@ -88,7 +88,7 @@
 		// var phish = new Project('../images/phish-quiz.png', 'https://b-ellis.github.io/Quiz/', 'https://github.com/b-ellis/Quiz');
 		// var toe = new Project('../images/tic-tac-toe.png', 'https://b-ellis.github.io/Tic-Tac-Toe/', 'https://github.com/b-ellis/Tic-Tac-Toe');
 		// var list = new Project('../images/shoppinglist.png', 'https://b-ellis.github.io/Shoppinglist/', 'https://github.com/b-ellis/Shoppinglist');
-		var jam = new Project('../images/jam-location.png', 'https://b-ellis.github.io/Jam-Location/', 'https://github.com/b-ellis/Jam-Location/tree/gh-pages');
+		var jam = new Project('../images/jam-loaction.png', 'https://b-ellis.github.io/Jam-Location/', 'https://github.com/b-ellis/Jam-Location/tree/gh-pages');
 		var Fullstack = new Project('../images/Fullstack-Capstone.png', 'https://thinkfulcapstone.herokuapp.com', 'https://github.com/b-ellis/Fullstack-capstone');
 		var node = new Project('../images/Node-Capstone.png','https://salty-hollows-61107.herokuapp.com/', 'https://github.com/b-ellis/node-capstone');
 		var React = new Project('../images/React-Capstone.png','https://react-capstone.herokuapp.com/','https://github.com/b-ellis/react-capstone');
